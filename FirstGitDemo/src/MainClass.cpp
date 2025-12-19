497a4673e212eb74d7daa7b0ede9869520cfec15
yannikwalz <yannikwalz@localhost>
2025-12-19 10:06:45 +0100

    initial commit

---
+++ FirstGitDemo/src/MainClass.cpp
@@ -0,0 +1,19 @@
+/*
+ * MainClass.cpp
+ *
+ *  Created on: 19.12.2025
+ *      Author: yannikwalz
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Git Test" << endl;
+
+	return 0;
+}
+
+
