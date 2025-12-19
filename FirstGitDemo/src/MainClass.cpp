0a2e59de29896f66c79602fda157b21e2a21b637
yannikwalz <ywalz@stud.hs-heilbronn.de>
2025-12-19 10:46:57 +0100

    now correct

--- FirstGitDemo/src/MainClass.cpp
+++ FirstGitDemo/src/MainClass.cpp
@@ -4,11 +4,7 @@
  *  Created on: 19.12.2025
  *      Author: yannikwalz
  *      Das ist mein erstes git Projekt
-<<<<<<< HEAD
  *      Kommentar mal anders
-=======
- * 		Eine Erweiterung des Kommentars
->>>>>>> refs/remotes/origin/master
  */
 
 #include <iostream>
